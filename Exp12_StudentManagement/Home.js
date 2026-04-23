b3e5f0243eb7169e633fbf4deb97b3ba76fda3d1
Akhil Bommu <akhilbommu17@gmail.com>
2026-04-23 19:55:50 +0530

    Create Home.js

---
+++ Exp12_StudentManagement/Home.js
@@ -0,0 +1,12 @@
+import React from "react"; 
+ 
+function Home() { 
+ return ( 
+   <div> 
+     <h2>Welcome to Student Management System</h2> 
+     <p>This system allows students to register and manage their details.</p> 
+   </div> 
+ ); 
+} 
+ 
+export default Home; 
