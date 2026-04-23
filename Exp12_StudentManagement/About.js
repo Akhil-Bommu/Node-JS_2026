e7695db40ae55386c2ed5e8ca2696133dc841e12
Akhil Bommu <akhilbommu17@gmail.com>
2026-04-23 19:59:07 +0530

    Create About.js

---
+++ Exp12_StudentManagement/About.js
@@ -0,0 +1,11 @@
+import React from "react"; 
+function About() { 
+return ( 
+<div> 
+<h2>About Student Management System</h2> 
+<p>This project is developed using React to demonstrate routing and component 
+structure.</p> 
+</div> 
+); 
+} 
+export default About;
